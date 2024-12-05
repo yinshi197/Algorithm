36279e0760e642dd7f6e29ea6fd141072b18fc7e
yinshi <1974126471@qq.com>
2024-12-05 22:42:49 +0800

    add:贪心算法

--- test.cpp
+++ test.cpp
@@ -10,80 +10,32 @@ using namespace std;
 
 class Solution {
 public:
-    bool isVaild(int row, int col, char val, vector<vector<char>>& borad)
-    {
-        for(int i = 0; i < 9; i++)  //判断是否行重复
+    int maxSubArray(vector<int>& nums) {
+        int ret = INT32_MIN;
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++)
         {
-            if(borad[row][i] == val) return false;
-        }
-
-        for(int j = 0; j < 9; j++)  //判断列重复
-        {
-            if(borad[j][col] == val) return false;
-        }
-
-        int startRow = (row / 3) * 3;
-        int startCol = (col / 3) * 3;
-        for(int i = startRow; i < startRow + 3; i++)
-        {
-            for(int j = startCol; j < startCol + 3; j++)
+            sum += nums[i];
+            if(sum > ret)
             {
-                if(borad[i][j] == val) return false;
+                ret = sum;
+            }
+            if(sum <= 0)
+            {
+                sum = 0;
             }
         }
 
-        return true;
-    }
-
-    bool backtracking(vector<vector<char>>& borad)
-    {
-        for(int i = 0; i < borad.size(); i++)
-        {
-            for(int j = 0; j < borad[0].size(); j++)
-            {
-                if(borad[i][j] != '.') continue;
-                for(char k = '1'; k <= '9'; k++)
-                {
-                    if(isVaild(i, j, k, borad))
-                    {
-                        borad[i][j] = k;
-                        if(backtracking(borad)) return true;
-                        borad[i][j] = '.';
-                    }
-                }
-                return false;   //9个数都试完了，都不行返回false
-            }
-        }
-
-        return true;
-    }
-
-    void solveSudoku(vector<vector<char>>& board) {
-        backtracking(board);
+        return ret;
     }
 };
 
 int main(int argc, char **argv)
 {   
-    vector<vector<char>> board = {{'5','3','.','.','7','.','.','.','.'},
-                                    {'6','.','.','1','9','5','.','.','.'},
-                                    {'.','9','8','.','.','.','.','6','.'},
-                                    {'8','.','.','.','6','.','.','.','3'},
-                                    {'4','.','.','8','.','3','.','.','1'},
-                                    {'7','.','.','.','2','.','.','.','6'},
-                                    {'.','6','.','.','.','.','2','8','.'},
-                                    {'.','.','.','4','1','9','.','.','5'},
-                                    {'.','.','.','.','8','.','.','7','9'}};
+    vector<int> nums = {-2,-1,-3,-4,-1,-2,-1,-5,-4};
     Solution *so = new Solution();
-    so->solveSudoku(board);
-    for(auto vec : board)
-    {
-        for(auto s : vec)
-        {
-            cout << s << " ";
-        }
-        cout << endl;
-    }
+    int ret = so->maxSubArray(nums);
+    cout << ret << endl;
 
     return 0;
 }
